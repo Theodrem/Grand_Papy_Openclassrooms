79cfeeb7fa55341442f8ea8623a5013b76a8b0a3
theot <theotim@outlook.fr>
2021-03-09 11:52:34 +0100

    Test geocoding

--- app/test/test_geocoding.py
+++ app/test/test_geocoding.py
@@ -1,16 +1,14 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import patch
 
 from app.api.geocoding import Geocoding
-from app.message import get_errors_response
 
 
 class TestGeocoding(unittest.TestCase):
     def setUp(self):
-        self.geo = Geocoding("paris")
-        self.get_errors_response = get_errors_response
-        self.geo.send_request = Mock()
-        self.response = {'results':
+        self.expected_geo_name = "paris"
+        self.geo = Geocoding(self.expected_geo_name)
+        self.expected = {'results':
             [{'address_components': [
                 {'long_name': 'Paris', 'short_name': 'Paris', 'types': ['locality', 'political']},
                 {'long_name': 'Paris', 'short_name': 'Paris',
@@ -27,26 +25,36 @@ class TestGeocoding(unittest.TestCase):
                                  'southwest': {'lat': 48.815573, 'lng': 2.224199}}},
                 'place_id': 'ChIJD7fiBh9u5kcRYJSMaMOCCwQ', 'types': ['locality', 'political']}],
             'status': 'OK'}
+        self.expected_lat = self.expected['results'][0]['geometry']['location']['lat']
+        self.expected_lng = self.expected['results'][0]['geometry']['location']['lat']
+        self.expected_address = self.expected['results'][0]['formatted_address']
 
-    def test_send_request(self):
-        self.assertEqual(self.geo.current, self.response)
+    @patch.object(Geocoding, 'send_request')
+    def test_send_request(self, mock_send_request):
+        mock_send_request.return_value = self.expected
+        result = self.geo.send_request()
+        self.assertEqual(self.expected, result)
 
-    def test_get_latitude(self):
-        try:
-            self.assertEqual(self.geo.current['results'][0]['location']['lat'], 48.856614)
-        except KeyError:
-            self.assertEqual(self.geo.current['results'][0]['geometry']['location']['lat'], 48.856614)
+    @patch.object(Geocoding, 'get_latitude')
+    def test_get_latitude(self, mock_get_latitude):
+        mock_get_latitude.return_value = self.expected_lat
+        result = self.geo.get_latitude()
+        self.assertEqual(self.expected_lat, result)
 
-    def test_get_longitude(self):
-        try:
-            self.assertEqual(self.geo.current['results'][0]['location']['lng'], 2.3522219)
-        except KeyError:
-            self.assertEqual(self.geo.current['results'][0]['geometry']['location']['lng'], 2.3522219)
+    @patch.object(Geocoding, 'get_longitude')
+    def test_get_latitude(self, mock_get_longitude):
+        mock_get_longitude.return_value = self.expected_lng
+        result = self.geo.get_longitude()
+        self.assertEqual(self.expected_lng, result)
 
-    def test_get_address(self):
-        try:
-            self.assertEqual(self.geo.current['results'][0]['formatted_address'], 'Paris, France')
-        except TypeError:
-            self.assertEqual(self.get_errors_response("no_found_geocoding"), get_errors_response("no_found_geocoding"))
+    @patch.object(Geocoding, 'get_address')
+    def test_get_latitude(self, mock_get_address):
+        mock_get_address.return_value = self.expected_address
+        result = self.geo.get_address()
+        self.assertEqual(self.expected_address, result)
+
+
+
+
 
 
