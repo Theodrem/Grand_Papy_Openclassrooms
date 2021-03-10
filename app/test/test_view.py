a2f9954d4d404b7b7b94b18d1000de09d3cc9572
theot <theotim@outlook.fr>
2021-03-10 12:56:09 +0100

    test view 2.1

--- app/test/test_view.py
+++ app/test/test_view.py
@@ -1,7 +1,10 @@
 import pytest
+import json
+
 from app import app
 from app.view import process
-import json
+from app.config import END_LIST, MESSAGE_LIST
+
 
 class TestView:
     """
@@ -16,11 +19,20 @@ class TestView:
             assert response.status_code == 200
             
     def test_process(self):
-        with process as c:
-            rv = c.post('/process', json={'input_user': "parsed_input", 'lat': "lat", 'lng': "lng",  # Return all data with json format
-                    'address': "address", "message": "message",
-                    'end_mess': "end_message", 'wiki': "mess_wiki"})
-            
-            json_data = rv.get_json()
-            assert verify_token(email, json_data['token'])
+        with app.test_client() as c:
+            parsed_input = "facebook"
+            lng = -122.1500939
+            lat = 37.4835791
+            address = "10 Hacker Way, Menlo Park, CA 94025, USA"
+            message = "Quelle aventure! Je te raconte mon petit:"
+            end_message =  "T'aimes mes histoires?"
+            mess_wiki = "Facebook City ou Zee-Town (Ville Facebook, ou Ville Z, comme Zuckerberg) est un important"
+            " projet de création de ville nouvelle-campus urbanisé-cité idéale d'environ 80 hectares. "
+            "Ce projet est lancé en 2012 par Mark Zuckerberg, PDG fondateur de la société américaine de réseautage social Facebook (entreprise),"
+            " à Menlo Park, dans la baie de San Francisco, de la Silicon Valley, en Californie, aux États-Unis,."
 
+            rv = c.post('/process', json={'input_user': parsed_input, 'lat': lat, 'lng': lng,  # Return all data with json format
+                    'address': address, "message": message,
+                    'end_mess': end_message, 'wiki': mess_wiki})
+
+            assert rv.status_code == 200
