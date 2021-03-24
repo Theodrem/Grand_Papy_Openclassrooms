9e1882de5a7f403708c7ec900237c4c59370edca
theot <theotim@outlook.fr>
2021-03-24 09:32:29 +0100

    Update 4.1

--- app/test/test_view.py
+++ app/test/test_view.py
@@ -1,31 +1,29 @@
 import unittest
 from mock import mock
-from app.api.geocoding import Geocoding
-from app.api.wiki import Wiki
 from app.parser import Parser
 from app.message import get_message, get_end_message
 from app.config import END_LIST, MESSAGE_LIST
 from app import app
 
-GEOCODING_DATA = {'results': [{'address_components': [{'long_name': '10', 'short_name': '10', 'types': ['street_number']},
-                                         {'long_name': 'Hacker Way', 'short_name': 'Hacker Way', 'types': ['route']},
-                                         {'long_name': 'Menlo Park', 'short_name': 'Menlo Park',
-                                          'types': ['locality', 'political']},
-                                         {'long_name': 'San Mateo County', 'short_name': 'San Mateo County',
-                                          'types': ['administrative_area_level_2', 'political']},
-                                         {'long_name': 'California', 'short_name': 'CA',
-                                          'types': ['administrative_area_level_1', 'political']},
-                                         {'long_name': 'United States', 'short_name': 'US',
-                                          'types': ['country', 'political']},
-                                         {'long_name': '94025', 'short_name': '94025', 'types': ['postal_code']}],
-                  'formatted_address': '10 Hacker Way, Menlo Park, CA 94025, USA',
-                  'geometry': {'location': {'lat': 37.4835791, 'lng': -122.1500939}, 'location_type': 'ROOFTOP',
-                               'viewport': {'northeast': {'lat': 37.4849280802915, 'lng': -122.1487449197085},
-                                            'southwest': {'lat': 37.4822301197085, 'lng': -122.1514428802915}}},
-                  'place_id': 'ChIJ_wMBpJe8j4ARVIR-F8nC79c',
-                  'plus_code': {'compound_code': 'FRMX+CX Menlo Park, CA, USA', 'global_code': '849VFRMX+CX'},
-                  'types': ['establishment', 'point_of_interest']}], 'status': 'OK'}
-
+GEOCODING_DATA = {
+    'results': [{'address_components': [{'long_name': '10', 'short_name': '10', 'types': ['street_number']},
+                                        {'long_name': 'Hacker Way', 'short_name': 'Hacker Way', 'types': ['route']},
+                                        {'long_name': 'Menlo Park', 'short_name': 'Menlo Park',
+                                         'types': ['locality', 'political']},
+                                        {'long_name': 'San Mateo County', 'short_name': 'San Mateo County',
+                                         'types': ['administrative_area_level_2', 'political']},
+                                        {'long_name': 'California', 'short_name': 'CA',
+                                         'types': ['administrative_area_level_1', 'political']},
+                                        {'long_name': 'United States', 'short_name': 'US',
+                                         'types': ['country', 'political']},
+                                        {'long_name': '94025', 'short_name': '94025', 'types': ['postal_code']}],
+                 'formatted_address': '10 Hacker Way, Menlo Park, CA 94025, USA',
+                 'geometry': {'location': {'lat': 37.4835791, 'lng': -122.1500939}, 'location_type': 'ROOFTOP',
+                              'viewport': {'northeast': {'lat': 37.4849280802915, 'lng': -122.1487449197085},
+                                           'southwest': {'lat': 37.4822301197085, 'lng': -122.1514428802915}}},
+                 'place_id': 'ChIJ_wMBpJe8j4ARVIR-F8nC79c',
+                 'plus_code': {'compound_code': 'FRMX+CX Menlo Park, CA, USA', 'global_code': '849VFRMX+CX'},
+                 'types': ['establishment', 'point_of_interest']}], 'status': 'OK'}
 
 WIKI_DATA_PAGE = {'batchcomplete': '', 'query': {'geosearch': [
     {'pageid': 10142861, 'ns': 0, 'title': 'Facebook City', 'lat': 37.481027, 'lon': -122.153898, 'dist': 439.5,
@@ -68,7 +66,7 @@ class TestView(unittest.TestCase):
     @mock.patch("app.api.geocoding.Geocoding")
     def test_process(self, mock_geo, mock_wiki):
         """
-        Check if the process view with the post method is correct
+        Check if the process view with the post method is correct.
         """
 
         mock_geo.return_value.send_request.return_value = GEOCODING_DATA
@@ -86,8 +84,9 @@ class TestView(unittest.TestCase):
 
         with app.test_client() as c:
             rv = c.post('/process', json={'input_user': p.transform_input(), 'lat': self.lat, 'lng': self.lng,
-                    'address': self.address, "message": message,
-                    'end_mess': end_message, 'wiki':  WIKI_DATA_DESC["query"]["pages"][str(10142861)]["extract"]})
+                                          'address': self.address, "message": message,
+                                          'end_mess': end_message,
+                                          'wiki': WIKI_DATA_DESC["query"]["pages"][str(10142861)]["extract"]})
 
             self.assertEqual(rv.status_code, 200)
 
@@ -100,7 +99,8 @@ class TestView(unittest.TestCase):
             mock_geo.return_value.get_address.return_value = self.address
             self.assertEqual(geo.get_address(), self.address)
 
-            mock_wiki.return_value.get_description.return_value = WIKI_DATA_DESC["query"]["pages"][str(10142861)]["extract"]
+            mock_wiki.return_value.get_description.return_value = WIKI_DATA_DESC["query"]["pages"][str(10142861)][
+                "extract"]
             self.assertEqual(wiki.get_description(), WIKI_DATA_DESC["query"]["pages"][str(10142861)]["extract"])
 
             self.assertEqual(p.transform_input(), self.parsed_input)
